01772ada04fca9dc0e337a58ad50b4ddda4019f2
gold Win <jsu2956@naver.com>
2021-02-19 17:56:29 +0900

    2021-02-19 17:56 상속에 관한테스트

--- TestCPP/TestCPP/main.cpp
+++ TestCPP/TestCPP/main.cpp
@@ -4,6 +4,34 @@ using namespace std;
 
 void main()
 {
-	//세번째 커밋!
-	cout << "Third Commit" << endl;
+	class Test1
+	{
+	public:
+		Test1()
+		{
+			cout << "부모 클래스 생성"<<this << endl;
+		}
+		void Start()
+		{
+			cout << "test! " <<this<< endl;
+		}
+	};
+
+	class Test2 : public Test1
+	{
+	public:	
+		Test2()
+		{
+			cout << "자식 클래스 생성" <<this<<endl;
+		}
+		void Start2()
+		{
+			cout << "Test2" <<this<<endl;
+		}
+	};
+	Test1 test1;
+	test1.Start();
+	Test2 test; 
+	test.Start();
+	test.Start2();
 }
